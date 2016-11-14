d012a84eefc5f550c06b39fdf2fb6f78e83e930a
Russell Henry <rjhenry1014@gmail.com>
2016-11-14 23:04:23 +0000

    Initial Files

---
+++ server.js
@@ -0,0 +1,37 @@
+var socket_io = require('socket.io');
+var http = require('http');
+var express = require('express');
+
+var app = express();
+app.use(express.static('public'));
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+var usersConnected = 0;
+
+io.on('connection', function (socket) {
+    
+    console.log('Client connected');
+    usersConnected++;
+    console.log('Number of connections:' + usersConnected);
+    
+    socket.on('add user', function(userName) {
+        socket.username = userName;
+        socket.broadcast.emit('user joined');
+    });
+
+    
+    socket.on('disconnect', function() {
+        console.log('Client disconnected.');
+        usersConnected--;
+        console.log('Number of connections:' + usersConnected);
+    });
+    
+    socket.on('message', function(message) {
+        console.log('Received message:', message);
+        socket.broadcast.emit('message', message);
+    });
+});
+
+server.listen(process.env.PORT || 8080);
